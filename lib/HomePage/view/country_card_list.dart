fa42caf602ffccedf55dd9d22f239fed32507021
arjun2020-py <arjunkoodathil@gmail.com>
2023-01-06 13:06:27 +0530

    change listview as future bulider for api data get purpose

--- lib/HomePage/view/country_card_list.dart
+++ lib/HomePage/view/country_card_list.dart
@@ -8,24 +8,34 @@ class CountryLanguageList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 62, 68, 74),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.r))),
-              color: const Color(0xff121416),
-              child: const ListTile(
-                title: Text(
-                  'Country name',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
+      body: StreamBuilder<Object>(
+        stream: null,
+        builder: (context, snapshot) {
+          return FutureBuilder<Object>(
+            future: null,
+            builder: (context, snapshot) {
+              return ListView.builder(
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.r))),
+                      color: const Color(0xff121416),
+                      child: const ListTile(
+                        title: Text(
+                          'Country name',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
           );
-        },
+        }
       ),
     );
   }
